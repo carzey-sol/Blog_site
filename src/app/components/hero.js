0e448d973ea6f3ca39741305c89e65958f37c699
Mukesh Bajgain <112241933+carzey-sol@users.noreply.github.com>
2024-11-19 11:30:33 +0545

    Hero.js redesigned

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -5,19 +5,25 @@ import Link from "next/link";
 
 const home = () => {
   return (
-    <div className=' md:ml-[22%] mt-2 '>
-      <div className="">
-      <Link href='/blog' >
-     {blogData.map((blog, index) => (
-      <div key={index} className=" mb-[32px] hover:scale-[1.05] transition-all duration-300">
-        <h1 className="font-bold text-xl mb-2 ">{blog.title}</h1>
-        <img  src={blog.image} />
+    // <div className='flex justify-center md:ml-[22%] mt-2 w-[650px] '>
+    //   <div className="">
+    //   <Link href='/blog' >
+    //  {blogData.map((blog, index) => (
+    //   <div key={index} className=" mb-[32px] hover:scale-[1.05] transition-all duration-300 bg-slate-400">
+    //     <h1 className="font-bold text-xl mb-2 ">{blog.title}</h1>
+    //     <img  src={blog.image} />
         
-        <p className="w-[50%] mt-2">{blog.description}</p>
-      </div>
-      ))}
-      </Link>
-      </div>
+    //     <p className="w-[50%] mt-2">{blog.description}</p>
+    //   </div>
+    //   ))}
+    //   </Link>
+    //   </div>
+    // </div>
+
+    <div className="flex flex-col text-center items-center mt-12 w-[300px] p-4 bg-slate-400 rounded-xl  ml-[12%] md:ml-[20%] md:hover:scale-[1.05] transition-all duration-300 ">
+      <h1 className="text-xl font-bold mb-2">Blog Title</h1>
+      <img src="./cs.png" className="w-[300px] mb-2 md:w"/>
+      <p className="w-[300px]"> kdmcjdasknsdadk alsdjsakd asjdnasd asjdn jaksnd jasnd asnjd asd </p>
     </div>
      
 
