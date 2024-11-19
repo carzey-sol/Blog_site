2ffa51a68a9c0e6beb2ea8f1ab9dae98e4e957c4
Mukesh Bajgain <112241933+carzey-sol@users.noreply.github.com>
2024-11-19 14:43:13 +0545

    Security implementation

--- src/app/lib/firebase.js
+++ src/app/lib/firebase.js
@@ -5,13 +5,13 @@ import {getAuth} from "firebase/auth"
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAB-PDEYwZwepcWXCnRSu6VgrE0AFuGiig",
-  authDomain: "blog-app-e6ae8.firebaseapp.com",
-  projectId: "blog-app-e6ae8",
-  storageBucket: "blog-app-e6ae8.firebasestorage.app",
-  messagingSenderId: "534483227245",
-  appId: "1:534483227245:web:1e29912406483c0a04a564"
-};
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
