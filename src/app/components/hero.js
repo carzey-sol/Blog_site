e7d731e588df806801fec4ce33970d6f77abf5c0
Mukesh Bajgain <112241933+carzey-sol@users.noreply.github.com>
2024-11-19 11:53:22 +0545

    Refined hero.js

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -7,10 +7,10 @@ const Home = () => {
   return (
     <div>
       {blogData.map((blog, index) => (
-        <div key={index} className="flex flex-col text-center items-center mt-12 w-[300px] md:w-[500px] p-4  rounded ml-[12%] md:ml-[70%] md:hover:scale-[1.05] transition-all duration-300">
-          <h1 className="text-xl font-bold mb-2">{blog.title}</h1>
-          <img src={blog.image} className="w-[300px] h-[300px] mb-2 md:w-[400px] md:h-[300px]" />
-          <p className="w-[300px]">{blog.description}</p>
+        <div key={index} className="flex flex-col text-left items-center mt-12 w-[300px] md:w-[500px] p-4 rounded ml-[12%] md:ml-[70%] md:hover:scale-[1.05] transition-all duration-300 ">
+          <h1 className="text-xl font-bold mb-2 hover:underline">{blog.title}</h1>
+          <img src={blog.image} className="w-[300px] h-[300px] mb-2 md:w-[400px] md:h-[300px] " />
+          <p className="w-[300px] ml-[25px] hover:underline">{blog.description}</p>
         </div>
       ))}
     </div>
